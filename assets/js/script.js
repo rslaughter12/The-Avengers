300a914befb8c2105df9f42fdbd4cef7db5f9309
Saritha Koduru <saritha.2708@gmail.com>
2023-03-26 15:45:49 -0700

    fixed some errors in js and added hover to watchlist

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,12 @@ const privateKey = "d86594961cf97e04100c02fa85ade28df00495a8";
 const publicKey = "d2fccda0b0d266ecc697390db7fb28c9";
 const ts = Number(new Date());
 var strHash = md5(ts + privateKey + publicKey);
-console.log(strHash);
 let watchlist = [];
+
 getWatchlist();
 
 
 function searchCharacter(input) {
-    // let input = $inputEl.val().trim();
     let character = '';
     if(input === 'Spider-Man'){
         character = 'Spider-Man (Peter Parker)';
@@ -26,43 +25,43 @@ function searchCharacter(input) {
     fetch(url) 
     .then(response => response.json())
     .then(function(data) {
-        console.log(data);
-        console.log(data.data);
         if(data.data.results.length === 0){
             $('#error').text(`Cannot find the Character "${input}"!`);
             $('#note').text('Note: Please spell the character right or try another one.');
             $('.modal-3').addClass('is-active');
         } else {
-        $('#character-title').text(data.data.results[0].name);
-        // $('#bio').text(`${data.data.results[0].name}'s Bio`);
-        let intro = 'Marvel Comics features the greatest superheroes and villains who exhibit the coolest powers and super-enhanced abilities.'
-        if(data.data.results[0].description){
-            $('#description').text(data.data.results[0].description);
-        }else {
-            $('#description').text(intro);
-        }
-        $('#character-img').attr('src', data.data.results[0].thumbnail.path + '.jpg');
-        $('#character-img').attr('alt', `picture of ${data.data.results[0].name}`);
-        const result = data.data.results[0].urls.filter(obj => obj.type === 'comiclink')
-        $('#comicsLink').attr('href', result[0].url);
-        listMovies(input);
+            hideElements(); 
+            showElements();
+            $('#character-title').text(data.data.results[0].name);
+            let intro = `Marvel characters are a diverse group of superheroes, villains, and other fantastical beings
+                         that populate the Marvel Universe. They possess a wide variety of powers and abilities, from 
+                         superhuman strength and speed to the ability to control the elements and manipulate reality 
+                         itself. These characters often have complex backstories and personalities that make them 
+                         relatable and engaging and their adventures are filled with drama, action, and high-stakes conflict.`
+            if(data.data.results[0].description){
+                $('#description').text(data.data.results[0].description);
+            }else {
+                $('#description').text(intro);
+            }
+            $('#character-img').attr('src', data.data.results[0].thumbnail.path + '.jpg');
+            $('#character-img').attr('alt', `picture of ${data.data.results[0].name}`);
+            const result = data.data.results[0].urls.filter(obj => obj.type === 'comiclink')
+            $('#comicsLink').attr('href', result[0].url);
+            listMovies(input);
         }
         $inputEl.val('');
     })
 }
 function listMovies(input) {
-    // const input = $inputEl.val().trim();
+    $('#movies').text('');
     const OMDB_URL =  `https://www.omdbapi.com/?s=${input}&apikey=${API_KEY}&type=movie`;
     fetch(OMDB_URL)
     .then(function(response){return response.json();})
     .then(function(data) {
-        console.log(data);
-        console.log(data.Response)
         if(data.Response === 'False'){
             $('#modal-4-note').text('Cannot fetch movies list. Try again!');
             $('.modal-4').addClass('is-active');
         } else{
-            $('#movies').text('');
             for(let i=0; i < data.Search.length; i++){
                 let text = data.Search[i].Title;
                 let liEl = $(`<li>${text}</li>`);
@@ -73,66 +72,64 @@ function listMovies(input) {
 
         }        
     })
-    .catch(function(error){console.log(error);});
 }
+
 function renderMovie(event) {
     event.stopPropagation();
     let e = event.target;
     if(e.matches(".movie")){
-        let movieTitle = e.textContent;
         let movieId = e.getAttribute('titleId');
-        console.log( typeof movieTitle);
         let movieURL = `http://www.omdbapi.com/?apikey=${API_KEY}&i=${movieId}`;
-        console.log(movieURL);
         fetch(movieURL)
         .then(response => response.json())
         .then(function(data) {
-            console.log(data);
             const $img = $("<img>");
-            try{
+            if(data.Poster != 'N/A'){
                 $img.attr('src', data.Poster);
-            }catch(err){
-                $('#modal-4-note').text('Cannot fetch the movie poster!');
-                $('.modal-4').addClass('is-active');
             }
             $('#movieTitle-1').text(data.Title)
             $('.desc-1').eq(0).text(data.Plot);
-            $('.desc-1').eq(1).text('Year: '+ data.Year);
-            $('.desc-1').eq(2).text('IMDB Rating: '+ data.imdbRating);
+            $('.desc-1').eq(1).text('Year:  '+ data.Year);
+            $('.desc-1').eq(2).text('Director:  '+ data.Director);
+            $('.desc-1').eq(3).text('Starring:  '+ data.Actors);
+            $('.desc-1').eq(4).text('IMDB Rating:  '+ data.imdbRating);
             $('#poster-1').append($img);
             $modal_1.addClass('is-active');
         })
-        .catch(function(error){console.log(error);});
     }
 }
+
 function renderMovieFromWatchlist(event) {
     showWatchlist();
     event.stopPropagation();
     let e = event.target;
     if(e.matches(".dropdown-item")){
         let movieTitle = e.textContent;
-        console.log( typeof movieTitle);
         let movieURL = `http://www.omdbapi.com/?apikey=${API_KEY}&t=${movieTitle}`;
-        console.log(movieURL);
         fetch(movieURL)
         .then(response => response.json())
         .then(function(data) {
-            console.log(data);
             const $img = $("<img>");
-            $img.attr('src', data.Poster);
+            if(data.Poster != 'N/A'){
+                $img.attr('src', data.Poster);
+            }
             $('#movieTitle-2').text(data.Title)
             $('.desc-2').eq(0).text(data.Plot);
-            $('.desc-2').eq(1).text('Year: '+ data.Year);
-            $('.desc-2').eq(2).text('IMDB Rating: '+ data.imdbRating);
+            $('.desc-2').eq(1).text('Year:  '+ data.Year);
+            $('.desc-2').eq(2).text('Director:  '+ data.Director);
+            $('.desc-2').eq(3).text('Starring:  '+ data.Actors);
+            $('.desc-2').eq(4).text('IMDB Rating:  '+ data.imdbRating);
             $('#poster-2').append($img);
             $modal_2.addClass('is-active');
         })
     }
 }
+
 function closeModal_1(){
     $modal_1.removeClass('is-active');
     $('#poster-1').children('img').remove();
 }
+
 function setWatchlist(){
     let movieTitle = $('#movieTitle-1').text();
     if(watchlist.includes(movieTitle)){
@@ -146,6 +143,7 @@ function setWatchlist(){
             $(".dropdown-content").append($pTag);
     }
 }
+
 function getWatchlist(){
     let dummy = JSON.parse(localStorage.getItem("watchlist"));
     if(dummy){
@@ -159,10 +157,8 @@ function getWatchlist(){
     }else{
         watchlist = [];
     }
-    
-    console.log(watchlist);
-    console.log(typeof watchlist);
 }
+
 function showWatchlist() {
     if($('.dropdown').attr('class') === 'dropdown'){
         $('.dropdown').addClass('is-active');
@@ -170,12 +166,11 @@ function showWatchlist() {
         $('.dropdown').removeClass('is-active');
     }
 }
+
 function removeFromWatchlist() {
     let movieTitle = $('#movieTitle-2').text();
-    console.log(movieTitle);
     if(watchlist.includes(movieTitle)){
         watchlist.splice(watchlist.indexOf(movieTitle),1);
-        console.log(watchlist);
         localStorage.setItem("watchlist", JSON.stringify(watchlist));
         $('.dropdown-content').text('');
         getWatchlist();
@@ -183,13 +178,15 @@ function removeFromWatchlist() {
         return;
     }
 }
+
 function closeModal_2(){
     $modal_2.removeClass('is-active');
     $('#poster-2').children('img').remove();
 }
+
 // Autocomplete widget
 $(function () {
-    var characterNames = [
+    let characterNames = [
       'Iron Man',
       'Black Widow',
       'Black Panther',
@@ -207,10 +204,29 @@ $(function () {
       source: characterNames,
     });
 });
+
+function hideElements() {
+    document.getElementById("introColumns").style.display = "none";
+}
+
+function showElements() {
+    document.getElementById("searchElements").classList.remove("is-hidden");
+}
+
+function clickCharacter(event) {
+    let character = event.target.dataset.character;
+    showElements();
+    searchCharacter(character);
+}
+
 $buttonEl.on("click", function(){
     let input = $inputEl.val().trim();
-    console.log(input);
-    searchCharacter(input);
+    if(!input){
+        $('#modal-4-note').text('Please enter a character to search!');
+        $('.modal-4').addClass('is-active');        
+    }else{
+        searchCharacter(input);
+    }
 });
 $thirdDiv.on("click", renderMovie);
 $('.cancel-1').on('click', closeModal_1);
@@ -235,16 +251,4 @@ $('#closeIcon-4').on('click', function(){
 });
 
 
-function hideElements() {
-    document.getElementById("introColumns").style.display = "none";
-  }
-  function showElements() {
-    document.getElementById("searchElements").classList.remove("is-hidden");
-  }
 
-  function clickCharacter(event) {
-    let character = event.target.dataset.character
-    console.log(character)
-    showElements()
-    searchCharacter(character);
-  }
